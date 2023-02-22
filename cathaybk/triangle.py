ce8835069a7f5f8f3b228e95e986c702e81228ff
chiaho <chiaho_lin@acbel.com>
2023-02-22 09:41:53 +0800

    V2_20230221

--- cathaybk/triangle.py
+++ cathaybk/triangle.py
@@ -1,12 +1,15 @@
-n = eval(input("請輸入正三角形邊長:"))
+n = int(input("請輸入正三角形邊長:"))
 
-for x in range(-n,0):
-  for y in range(-n,n):
-    if abs(x)+abs(y) == n -1:
-        print("*",end=' ')
-    else:
-        print(" ",end=' ')
-  print()
 for i in range(n):
-    print('  ',end = '')
-    print("*",end= ' ')
+  print(" "*(n-i-1), end="")
+  if i==0:
+    print("*"*(2*i+1))
+  elif 0<i and i<n-1:
+    print("*"+" "*(2*i-1)+"*")
+  else:
+    for j in range(2*n-1):
+      if j%2==0:
+        print("*", end="")
+      else:
+        print(" ", end="")
+    print()
